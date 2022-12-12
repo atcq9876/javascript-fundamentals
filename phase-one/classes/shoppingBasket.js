00a4d30b9c717d9fa9ab8150c7832b7b6344322f
Andy <ajscottlewis@gmail.com>
2022-12-12 19:56:26 +0000

    Complete stage 12 challenge; complete phase 1

--- phase-one/classes/shoppingBasket.js
+++ phase-one/classes/shoppingBasket.js
@@ -1,18 +1,57 @@
 class ShoppingBasket {
 
   constructor() {
-    this.items = []
+    this.items = [];
   }
 
   addItem(item) {
-    return items.push(item);
+    return this.items.push(item);
   }
-
+  
   getTotalPrice() {
-    return items.map((item) => {
-      return item.price;
-    })
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      let totalPrice = 0
+      this.items.forEach((item) => {
+        totalPrice += item.price
+      })
+      return totalPrice;
+    } 
   }
 }
 
 module.exports = ShoppingBasket;
+
+// class Candy {
+
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+
+//   getPrice() {
+//     return this.price;
+//   }
+// }
+
+
+// const candy = new Candy('Mars', 4.99);
+
+// console.log(candy.getName());
+// console.log(candy.getPrice());
+
+// const basket = new ShoppingBasket();
+// console.log(basket.getTotalPrice());
+
+// basket.addItem(candy);
+// console.log(basket.getTotalPrice());
+
+// basket.addItem(new Candy('Skittle', 3.99));
+// basket.addItem(new Candy('Skittle', 3.99));
+
+// console.log(basket.getTotalPrice());
