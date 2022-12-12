9b553fda0ad14b2fdadcd0265e1f4c2fffb66e39
Andy <ajscottlewis@gmail.com>
2022-12-12 15:06:08 +0000

    Complete stage 9 (arrays) of phase 1

---
+++ phase-one/arrays/batches.js
@@ -0,0 +1,14 @@
+const addToBatch = (array, num) => {
+  if (array.length >= 5) {
+    console.log(array);
+  } else {
+    console.log(array.concat(num));
+  }
+};
+
+addToBatch([1], 3);
+addToBatch([1, 2, 3], 4);
+addToBatch([], 8);
+addToBatch([1, 2, 3, 4, 5], 6);
+addToBatch([1, 2, 3, 4, 5, 6], 7);
+addToBatch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10);
---
+++ phase-one/arrays/names.js
@@ -0,0 +1,6 @@
+const names = ['Andy', 'James', 'Scott', 'Lewis'];
+console.log(names.length);
+console.log(names[1]);
+
+const namesNew = names.concat('Jimmy');
+console.log(namesNew);
