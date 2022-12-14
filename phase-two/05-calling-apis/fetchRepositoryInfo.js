2cad4e1ef2f74213339ef4a2aadbabf903434083
Andy <ajscottlewis@gmail.com>
2022-12-14 12:30:16 +0000

    Refactor exercise 3

--- phase-two/05-calling-apis/fetchRepositoryInfo.js
+++ phase-two/05-calling-apis/fetchRepositoryInfo.js
@@ -1,12 +1,15 @@
 const { get } = require('callback-fetch');
 
 const fetchRepositoryInfo = (repoPath, callbackFunction) => {
-  url = 'https://api.github.com/repos/' + repoPath;
-  (get(url, callbackFunction));
+  let url = 'https://api.github.com/repos/' + repoPath;
+  get(url, (response) => {
+    let responseObject = JSON.parse(response);
+    return callbackFunction(responseObject);
+  })
 };
 
 module.exports = fetchRepositoryInfo;
 
-fetchRepositoryInfo('sinatra/sinatra', (receivedResponse) => {
-  console.log(JSON.parse(receivedResponse));
-});
+// fetchRepositoryInfo('sinatra/sinatra', (receivedResponse) => {
+//   console.log(receivedResponse);
+// });
