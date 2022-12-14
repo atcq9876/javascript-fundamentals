172f5b528ade3a2e0dc731a0cf5fe4e156c8a119
Andy <ajscottlewis@gmail.com>
2022-12-14 14:00:02 +0000

    Comment out extra code

--- phase-two/05-calling-apis/github.js
+++ phase-two/05-calling-apis/github.js
@@ -1,16 +1,16 @@
 const { get } = require('callback-fetch');
 
-class GithubClient {
-  constructor() {}
+// class GithubClient {
+//   constructor() {}
 
-  fetchRepositoryData(repoPath, callbackFunction) {
-    let url = 'https://api.github.com/repos/' + repoPath;
-    get(url, (response) => {
-      let responseObject = JSON.parse(response);
-      return callbackFunction(responseObject);
-    });
-  }
-}
+//   fetchRepositoryData(repoPath, callbackFunction) {
+//     let url = 'https://api.github.com/repos/' + repoPath;
+//     get(url, (response) => {
+//       let responseObject = JSON.parse(response);
+//       return callbackFunction(responseObject);
+//     });
+//   }
+// }
 
 class Github {
   constructor(client) {
@@ -31,13 +31,13 @@ class Github {
 
 module.exports = Github;
 
-const client = new GithubClient();
+// const client = new GithubClient();
 
-// We inject the instance of `GithubClient`:
-const github = new Github(client);
+// // We inject the instance of `GithubClient`:
+// const github = new Github(client);
 
-// This method will delegate to `GithubClient.fetchRepositoryData()`
-github.fetch('sinatra/sinatra');
+// // This method will delegate to `GithubClient.fetchRepositoryData()`
+// github.fetch('sinatra/sinatra');
 
-// And after a few moments, this should return a JS object with the repo information.
-github.getRepoData();
+// // And after a few moments, this should return a JS object with the repo information.
+// github.getRepoData();
