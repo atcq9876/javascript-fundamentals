c5977bb9350c2b33e698cb81ea8006d8b2ddf1e6
Andy <ajscottlewis@gmail.com>
2022-12-15 10:39:47 +0000

    Add displayWeather() function

--- phase-two/06-weather-api/weather.js
+++ phase-two/06-weather-api/weather.js
@@ -14,15 +14,29 @@ class Weather {
   getWeatherData() {
     return this.data;
     // setTimeout(() => { return this.data }, 500);
-    // setTimeout(() => { console.log(this.data) }, 500);
+    // setTimeout(() => { console.log(this.data) }, 1000);
+  }
+
+  displayWeather() {
+    const display = {
+      City: this.data.name,
+      Weather: this.data.weather[0].main,
+      Temperature: this.data.main.temp,
+      Feels_like: this.data.main.feels_like,
+      Humidity: this.data.main.humidity
+    }
+    return display;
   }
 }
 
 module.exports = Weather;
 
-// const client = new WeatherClient();
-// const weather = new Weather(client);
+const client = new WeatherClient();
+const weather = new Weather(client);
 
-// weather.fetch('Bristol');
+weather.fetch('London');
 
-// weather.getWeatherData();
+// setTimeout(() => { console.log(weather.getWeatherData()) }, 1000);
+// console.log(weather.getWeatherData());
+
+setTimeout(() => { console.log(weather.displayWeather()) }, 1000);
